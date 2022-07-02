28f2813c7ab01456a2b2552f3e03bb8d5c4717ee
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-02 09:45:21 -0300

    test(task.service): create task

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -15,4 +15,32 @@ describe('TaskService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () => {
+    it('valid task', async () => {
+      const task = {
+        name: 'testTask',
+        description: 'Test description',
+      };
+      const createdTask = await service.create(task);
+      expect(createdTask).toMatchObject(createdTask);
+    });
+
+    it('missing fields', async () => {
+      const task = {
+        name: 'testTask',
+      };
+      const createdTask = await service.create(task);
+      expect(createdTask).toBeInstanceOf(Error);
+    });
+
+    it('invalid fields', async () => {
+      const task = {
+        name: 1,
+        description: {},
+      };
+      const createdTask = await service.create(task);
+      expect(createdTask).toBeInstanceOf(Error);
+    });
+  });
 });
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TaskService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  async create(createTaskDto: CreateTaskDto): Promise<Task | Error> {
+    return createTaskDto;
   }
 
-  findAll() {
-    return `This action returns all task`;
+  async findAll() {
+    return [];
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async findOne(id: number) {
+    return {};
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    return {};
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: number) {
+    return {};
   }
 }
