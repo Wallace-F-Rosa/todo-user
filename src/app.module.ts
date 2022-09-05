87dc4abada0441eadf3f1d4cc1727a3d2c28cbb0
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-09-05 20:33:22 -0300

    feat(app.module): load test env when NODE_ENV is 'test'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        process.env.NODE_ENV === 'test'
+          ? `.env.${process.env.NODE_ENV}`
+          : `.env`,
+      ],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
