c5ff8815b74080592da361d3777fedf1cb4b26d3
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-09-05 20:26:22 -0300

    feat: added prisma service and user id

---
+++ prisma/migrations/20220905232444_user_id/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - The primary key for the `Role` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[name]` on the table `Role` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - The required column `id` was added to the `Role` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+  - The required column `id` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_RoleToUser" DROP CONSTRAINT "_RoleToUser_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_RoleToUser" DROP CONSTRAINT "_RoleToUser_B_fkey";
+
+-- AlterTable
+ALTER TABLE "Role" DROP CONSTRAINT "Role_pkey",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "Role_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "_RoleToUser" ADD CONSTRAINT "_RoleToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RoleToUser" ADD CONSTRAINT "_RoleToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
